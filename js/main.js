85f6391625ad47cd863093e73914ce7f0af62152
ErebusAngelo <dangelo.erebus@gmail.com>
2026-09-01 18:33:35 -0300

    contacto

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function renderHeader() {
       <nav class="nav-menu flex gap-sm" aria-label="Navegación principal">
         <a href="nosotros.html" class="nav-link ${isNosotros ? 'active' : ''}">Nosotros</a>
         <a href="index.html#soluciones" class="nav-link ${isSoluciones ? 'active' : ''}">Soluciones</a>
-        <a href="index.html#contacto" class="nav-link ${isContacto ? 'active' : ''}">Contacto</a>
+        <a href="contacto.html" class="nav-link ${isContacto ? 'active' : ''}">Contacto</a>
       </nav>
     </div>
   `;
@@ -43,7 +43,7 @@ function renderFooter() {
         <a href="index.html">Inicio</a>
         <a href="nosotros.html">Nosotros</a>
         <a href="index.html#soluciones">Soluciones</a>
-        <a href="index.html#contacto">Contacto</a>
+        <a href="contacto.html">Contacto</a>
       </div>
 
       <div class="footer-col">
